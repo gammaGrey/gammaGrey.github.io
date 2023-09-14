7925f6be631be713d1f07bdce8cfaf4a98abb07f
Tomas J <125275625+gammaGrey@users.noreply.github.com>
2023-09-14 09:11:11 +0100

    fix missing space in About page header

--- JS/email_script.js
+++
@@ -1,9 +0,0 @@
-function sendEmailForm (message, sender = "anon") {
-    //full date and time (and timezone)
-    //e.g. "Wed Sep 13 2023 11:13:09 GMT+0100 (British Summer Time)"
-    const timeStamp = new Date(Date.now()).toString();
-
-    
-}
-
-sendEmailForm();
